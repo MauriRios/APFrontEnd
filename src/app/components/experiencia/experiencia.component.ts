0b7dccbaac5a02d7ded3a5f22aef80913e2403d2
MauriRios <87584747+MauriRios@users.noreply.github.com>
2022-05-29 00:18:56 -0300

    asd

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -1,89 +1,80 @@
 import { Component, OnInit } from '@angular/core';
-import { Experiencia } from 'src/app/model/experiencia.model';
+import { Experiencia, Experiencias } from 'src/app/model/experiencia.model';
 import { ExperienciaService } from 'src/app/service/experiencia.service';
-
 
 @Component({
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
-  styleUrls: ['./experiencia.component.css']
+  styleUrls: ['./experiencia.component.css'],
 })
 export class ExperienciaComponent implements OnInit {
+  experiencia: Experiencia = new Experiencia(0, '', '', '', '');
+  experiencias: Experiencias[] = [];
 
- experiencia: Experiencia = new Experiencia(0,"","","","");
- experiencias: Experiencia [] = [];
-  
-  constructor(private experienciaService: ExperienciaService,
-
-  ) { }
+  constructor(private experienciaService: ExperienciaService) {}
 
   ngOnInit(): void {
-    this.tomarId(this.experiencia);
     this.cargarExperiencias();
-   
+
     // this.experienciaService.getExperiencia().subscribe(data => {this.experiencia = data})
+  }
+
+  cargarExperiencias() {
+    this.experienciaService.getExperiencias().subscribe((data) => {
+      this.experiencias = data;
+      
+      console.log(this.experiencias)
+    });
 
   }
 
-  cargarExperiencias(){
-    this.experienciaService.getExperiencias().subscribe(
-      data => {
-        this.experiencias = data;
-      });
-  }
-
-   Agregar(experiencia: Experiencia) {
+  Agregar(experiencia: Experiencia) {
     this.experienciaService.createExperiencia(experiencia).subscribe();
-       alert("Experiencia creada...");
-    window.location.reload( );
+    alert('Experiencia creada...');
+    window.location.reload();
 
-      console.log(Experiencia)
+    console.log(Experiencia);
   }
 
-   Eliminar(experiencias:Experiencia){     
-    this.experienciaService.deleteExperiencia(experiencias).subscribe(data=>{       
-           this.experiencias = this.experiencias.filter(p =>
-             p !== experiencias);
-                  alert("Experiencia eliminada...");    
-     })
+  Eliminar(experiencias: Experiencia): void {
+    this.experienciaService
+      .deleteExperiencia(experiencias)
+      .subscribe((data) => {
+        this.experiencias = this.experiencias.filter((p) => p !== experiencias);
+        alert('Experiencia eliminada...');
+      });
 
-     console.log(experiencias)
-             
+    console.log(experiencias);
   }
 
-  getValue(id: number) {
-    this.experiencia.id = this.experiencia.id;
-    console.log(this.experiencia.id)
-  }
+  
+  Actualizar(experiencia: Experiencia) {
+    this.experienciaService.editExperiencia(experiencia).subscribe((data) => {
+      this.experiencia = data;
+      alert('Se Actualizo con Exito...!!!');
 
+    console.log(this.experiencia);
+
+    });
+    
+  }
+  
   Editar(): void {
-    let id = localStorage.getItem("id");
-         this.experienciaService.getExperiencia(+id).subscribe(data=>{
-         this.experiencia = data;
+    let id = localStorage.getItem('id');
+    this.experienciaService.getExperiencia(+id).subscribe((data) => {
+      this.experiencia = data;
 
-         console.log(id);
-})
-}  
+      console.log(id);
+    });
+  }
 
-Actualizar(experiencia: Experiencia){
-  this.experienciaService.editExperiencia( experiencia).subscribe
-  (data => { this.experiencia = data;
-alert("Se Actualizo con Exito...!!!")
-})
-
-console.log(this.experiencia)
+  tomarId(experiencia: Experiencia): void {
+    localStorage.setItem('id', experiencia.id.toString());
+    console.log(localStorage);
+  }
 }
 
-  tomarId(experiencia : Experiencia ): void{   
-     localStorage.setItem("id", experiencia.id.toString());  
-                     
-       }
-  }
-
-
-  
-
-  
-
-
-
+// getValue(id: number) {
+//   this.experiencia.id = this.experiencia.id;
+//   console.log(this.experiencia.id)
+// }
--- src/app/service/experiencia.service.ts
+++ src/app/service/experiencia.service.ts
@@ -4,34 +4,30 @@ import { Observable } from 'rxjs';
 import { Experiencia } from '../model/experiencia.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ExperienciaService {
+  URL = 'http://localhost:8080/experiencia';
 
- 
-  URL = 'http://localhost:8080/experiencia/';
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-    public getExperiencias(): Observable<Experiencia[]>{
-    return this.http.get<Experiencia[]>(this.URL + 'traer/');
+  public getExperiencias(): Observable<Experiencia[]> {
+    return this.http.get<Experiencia[]>(this.URL + '/traer');
   }
 
-    public getExperiencia(id:number) { 
-      return this.http.get<Experiencia>(this.URL + "traer/" + id);
-    }
-
-    public createExperiencia(experiencia:Experiencia){
-    return this.http.post<Experiencia>(this.URL + 'crear/', experiencia);
-  }
- 
-    public deleteExperiencia(experiencia:Experiencia){ 
-    return this.http.delete<Experiencia>(this.URL+"borrar/"+ experiencia.id );   
+  public getExperiencia(id: number) {
+    return this.http.get<Experiencia>(this.URL + '/' + id);
   }
 
-    public editExperiencia(experiencia:Experiencia){
-    return this.http.put<Experiencia>(this.URL +'editar/' + experiencia.id, experiencia)
-       
-     }
+  public createExperiencia(experiencia: Experiencia) {
+    return this.http.post<Experiencia>(this.URL + '/crear/', experiencia);
+  }
 
+  public deleteExperiencia(experiencia: Experiencia) {
+    return this.http.delete<Experiencia>(this.URL + '/borrar/' + experiencia.id);
+  }
+
+  public editExperiencia(experiencia: Experiencia) {
+    return this.http.put<Experiencia>(this.URL + '/editar/' + experiencia.id,experiencia);
+  }
 }
